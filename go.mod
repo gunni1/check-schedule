4ee5259af2a2177b4569523f6b19a406a44fadc4
gunni <gunni1111@gmail.com>
2019-05-29 22:11:41 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module schedulecheck
+
+go 1.12
---
+++ xml.go
@@ -0,0 +1,19 @@
+package main
+
+import "encoding/xml"
+
+type Vp struct {
+	XMLName xml.Name `xml:"vp"`
+	Head    Head     `xml:"kopf"`
+}
+
+type Head struct {
+	Titel      string `xml:"titel"`
+	UploadDate string `xml:"datum"`
+	Info       Info   `xml:"kopfinfo"`
+}
+
+//Kopfinfo im XML
+type Info struct {
+	ChangesTeacher string `xml:"aenderungl"`
+}
